207535ccfa232d2029ace0c9fb37e82f9d8bd05c
ECED3901 Group 1 <owensullivan@dal.ca>
2025-03-09 15:56:11 -0300

    Outline of the final software, prepared for map interpretation and subprocess functions.

--- eced3901/eced3901_dt1.py
+++ eced3901/eced3901_dt1.py
@@ -96,8 +96,8 @@ class LaserDataInterface(object):
 
 """
  - highpassFilter()
-    Function used to apply a high-pass filter
-    to the lidar data. The low distance values
+    function used to apply a high-pass filter
+    to the lidar data. the low distance values
     must be filtered to apply accurate data processing.
 """
 def highpassFilter(value):
@@ -112,9 +112,9 @@ def highpassFilter(value):
 
 """
  - NoneFilter()
-    Function used to apply a filter that
+    function used to apply a filter that
     removes 'None' values from the lidar data.
-    The 'None' values are generated from lidar
+    the 'None' values are generated from lidar
     distances that are too large, and must be
     filtered to apply accurate data processing.
 """
@@ -130,7 +130,7 @@ def NoneFilter(value):
 
 """
  - min_lidar_distance()
-    Function used to find the minimum value in the lidar array,
+    function used to find the minimum value in the lidar array,
     using the high-pass filter to avoid false readings.
 """
 def min_lidar_distance(data):
@@ -152,6 +152,12 @@ class NavigateCourse(Node):
     def __init__(self):
         
         super().__init__('navigate_course')
+
+        self.cell = start_cell
+
+        self.attribute_check_complete = 0
+
+        self.lidar_tolerance = 0.02
 
         # Subscribe to odometry
         self.sub_odom = self.create_subscription(
@@ -186,6 +192,7 @@ class NavigateCourse(Node):
     def timer_callback(self):
         """Timer callback for 10Hz control loop"""
 
+        self.primaryProcess()
         self.lidar_test() 
 
     def odom_callback(self, msg):
@@ -213,36 +220,221 @@ class NavigateCourse(Node):
         self.pub_vel.publish(msg)
         super().destroy_node()
 
+    """
+     - lidar_test()
+        function used to test lidar functionality
+        and view range array.
+    """
     def lidar_test(self):
 
         lr = self.ldi.get_range_array(0.0)
-
+        
         if lr is None:
             return
 
-        self.get_logger().info(str(lr))
+        i = [1, 95, 190, 285]
+
+        lr2 = [lr[j] for j in i]
+
+        self.get_logger().info(str(lr2))
+
+    """
+     - pullLidar()
+        function used to select and return values from
+        lidar range array.
+    """
+    def pullLidar(self, *args):
+
+        # read 380 lidar values
+        laser_ranges = self.ldi.get_range_array(0.0)
+
+        # select pull values from range array
+        pull_values = [laser_ranges[arg] for arg in args if 0 <= arg < 380]
+        
+        return pull_values
+    """
+     - primaryProcess()
+        function repeated by the timer callback
+        which directs the primary process of the
+        robot.
+    """
+    def primaryProcess(self):
+
+        # determine if the attributes have been checked
+        if not self.attribute_check_complete:
+
+            checkCell(self.cell)
+
+        # determine the current NWSE lidar values
+        NWSE_lidar = self.pullLidar(D["NORTH"], D["WEST"], D["SOUTH"], D["EAST"])
+
+        # ensure proper values were returned
+        if not NWSE_lidar:
+
+            return
+
+        # compare current NWSE lidar values with neighbour lidar values
+        new_cell = checkNN(self.cell, NWSE_lidar, nearest_neighbour)
+
+        if new_cell:
+
+            self.cell = new_cell
+
+            self.attribute_check_complete = 0
 
 
 """
  ========== FINAL FUNCTION DEFINITIONS ==========
 """
 
+"""
+ - read_map()
+    function used to load map based on the specified
+    layers in the 'layers' file.
+"""
 def read_map():
     
     with open(map_directory + "/layers", "r") as file:
 
         map_layers = file.read().split()
 
-    return np.array([np.loadtxt(map_directory + filename) for filename in map_layers])
+    return np.array([np.loadtxt(map_directory + filename) for filename in map_layers]), map_layers
+
+"""
+ - checkLidar()
+    function used to compare two lidar array
+    within a set tolerance.
+"""
+def checkLidar(lidar1, lidar2): 
+
+    for l1, l2 in zip(lidar1, lidar2):
+        
+        if l1 is None or l2 is None:
+
+            return False
+
+        if abs(l1 - l2) > self.lidar_tolerance:
+
+            return False
+
+    return True
+
+
+"""
+ - checkNN()
+    function used to determine the next cell location
+    by comparing current lidar values to the lidar
+    values stored in the nearest neighbours.
+"""
+def checkNN(cell, lidar, directions):
+
+    # loop through provided directions
+    for direction in directions:
+
+        # find the neighbour in each direction
+        new_cell = list(map(lambda i, j: i + j, cell, direction))
+            
+        # query the lidar values for the new cell
+        new_cell_lidar = [course_map[map_layers.index(d)][new_cell[0]][new_cell[1]] for d in NWSE]
+
+        # determine if current lidar values match map lidar values
+        if checkLidar(lidar, new_cell_lidar):
+
+            # update current cell
+            return new_cell
+
+    return False
+
+"""
+ - checkCell()
+    function used to check the cell attributes
+    at given coordinates.
+"""
+def checkCell(cell):
+
+    for attribute in attribute_priority:
+
+        if attribute in map_layers:
+
+            if course_map[map_layers.index(attribute)][cell[0]][cell[1]]:
+
+                subProcess[attribute_priority.index(attribute)]()
+    
+    self.attribute_check_complete = 1
+
+subProcess = [lootProcess, magnetProcess, rfidProcess, qrProcess, safeProcess, indianaProcess, cageProcess]
+
+def lootProcess():
+
+    pass
+
+def magnetProcess():
+
+    pass
+
+def rfidProcess():
+
+    pass
+
+def qrProcess():
+
+    pass
+
+def safeProcess():
+
+    pass
+
+def indianaProcess():
+
+    pass
+
+def cageProcess():
+
+    pass
 
 """
  ========== FINAL GLOBAL VARIABLES ==========
 """
 
-current_directory = os.getcwd()
+"""
+ read map from sub-directory.
+ map/ must sit parallel to eced3901_dt1.py.
+ map/ must contain a 'layers' file, with
+ a list of map layers.
+"""
+# current_directory = os.getcwd()
+current_directory = "/home/student/ros2_ws/src/eced3901/eced3901" 
+
 map_directory = current_directory + "/map/"
 
-course_map = read_map()
+course_map, map_layers = read_map()
+
+"""
+ enum to align cardinal directions
+ with 380 element LIDAR array.
+"""
+D = {
+    "NORTH": 1,
+    "NORTH-WEST": 47,
+    "WEST": 95,
+    "SOUTH-WEST": 142,
+    "SOUTH": 190,
+    "SOUTH-EAST": 237,
+    "EAST": 285,
+    "NORTH-EAST": 332
+}
+#NWSE = [ "NORTH", "WEST", "SOUTH", "EAST" ]
+NWSE = [ "north", "west", "south", "east" ]
+
+"""
+ direction tables to check surrounding cells
+"""
+nearest_neighbour = [ [0, -1], [-1, 0], [0, 1], [1, 0] ]
+next_nearest_neighbour = [ [-1, -1], [1, -1], [1, 1], [-1, 1] ]
+
+start_cell = [8, 9]
+
+attribute_priority = [ "loot", "magnet", "rfid", "qr", "safe", "indiana", "cage"]
 
 """
  ========== MAIN ==========
@@ -257,6 +449,7 @@ def main(args=None):
     rclpy.spin(navigate_course)
 
     navigate_square.destroy_node()
+
     rclpy.shutdown()
     
 if __name__ == '__main__':
